5af9c0d556d7b87f3dcaf030ce7594a74d0524b3
Krutik Raut <58563133+reboot13-git@users.noreply.github.com>
2021-11-01 15:18:06 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+window.onkeypress = (e) => {
+    document.getElementById('kp').style.display = 'none'
+    document.getElementById('whichkey').innerText= e.code;
+    document.getElementById('key').innerText = e.key
+    document.getElementById('location').innerText = e.location
+    document.getElementById('which').innerText = e.which
+    document.getElementById('code').innerText = e.code
+}
